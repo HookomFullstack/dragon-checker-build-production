3244290f66ca14dd7d8eaf6f5126cc80ed845d38
MiguelFullStack <miguel.fullstack@gmail.com>
2023-01-20 14:48:37 -0400

    server automatico

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const { server } = require('./build');
 const { tipo, productoPersonalizado, tortaPersonalizada } = require('./config/config');
 
 const axios = require('axios').default;
-
+server()
 axios.post('http://localhost:3000/',
 {
     tipo,
