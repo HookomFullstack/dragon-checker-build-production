8afb1a8d2d6c4c7a91f1b832151c4ee6851bc2bf
MiguelFullStack <miguel.fullstack@gmail.com>
2023-03-02 04:20:24 -0400

    giftcard add method

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
-// foto, manual-login, manual-torta, manual-foto 
+// foto, manual-login, manual-torta, manual-foto, gifcard
 // https://www.walmart.com/ip/seort/10448549
 
-const tipo = 'foto';
+const tipo = 'giftcard';
 
 // SI QUIERES UN PRODUCTO DIFERENTE A LA LATA DE MAIZ, COLOCA UN PRODUCTO AQUI
 const productoPersonalizado = '';
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { server } = require('./build');
-const { tipo, productoPersonalizado } = require('./config/config');
+const { tipo, productoPersonalizado } = require('./config/config.js');
 const axios = require('axios').default;
 
 server().then(e => axios.post(`http://localhost:${e}/`, {tipo,productoPersonalizado}) )
